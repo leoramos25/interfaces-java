0eb9f4be01bd7491feba8055f07233744af67bf8
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-11 18:23:22 -0300

    exercise solution with interface

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,6 +1,7 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
+    @Override
     public double tax(double amount) {
         if (amount <= 100.0) {
             return amount * 0.2;
