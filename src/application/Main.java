c5b8c9810e00e701000c381826570565ff83840b
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-11 22:30:56 -0300

    exercise for pratice interface usage

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,41 +1,42 @@
 package application;
 
-import model.entities.CarRental;
-import model.entities.Vehicle;
-import model.services.BrazilTaxService;
-import model.services.RentalService;
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Entre com os dados do aluguel");
-        System.out.print("Modelo do carro: ");
-        String carModel = scanner.nextLine();
-        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
-        LocalDateTime startDate = LocalDateTime.parse(scanner.nextLine(), dateFormatter);
-        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
-        LocalDateTime finishDate = LocalDateTime.parse(scanner.nextLine(), dateFormatter);
-        System.out.print("Entre com o preço por hora: ");
-        double pricePerHour = scanner.nextDouble();
-        System.out.print("Entre com o preço por dia: ");
-        double pricePerDay = scanner.nextDouble();
+        System.out.println("Entre os dados do contrato:");
+        System.out.print("Numero: ");
+        int number = scanner.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(scanner.next(), dateFormatter);
+        System.out.print("Valor do contrato: ");
+        double totalValue = scanner.nextDouble();
 
-        CarRental carRental = new CarRental(startDate, finishDate, new Vehicle(carModel));
-        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
-        rentalService.processInvoice(carRental);
+        Contract contract = new Contract(number, date, totalValue);
 
-        System.out.println("FATURA: ");
-        System.out.println("Pagamento básico: " + String.format("R$ %.2f", carRental.getInvoice().getBasicPayment()));
-        System.out.println("Imposto: " + String.format("R$ %.2f", carRental.getInvoice().getTax()));
-        System.out.println("Pagamento total: " + String.format("R$ %.2f", carRental.getInvoice().getTotalPayment()));
+        System.out.print("Entre com o numero de parcelas: ");
+        int manyInstallments = scanner.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(contract, manyInstallments);
+
+        System.out.println("Parcelas: ");
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(installment);
+        }
 
         scanner.close();
     }
